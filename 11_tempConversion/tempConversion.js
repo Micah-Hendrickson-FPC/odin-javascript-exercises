d3266d41c7c7f2ec9c597fe2b96d78935e7e47f2
Micah-Hendrickson-FPC <micah.hendrickson@fivepackcreative.com>
2025-06-11 16:19:53 -0500

    Create functions to convert between F & C

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahr) {
+  //Converting from f to c is (f - 32) * 5 / 9
+  let cels = Math.round(((fahr - 32) * 5 / 9) * 10) /10;
+  return cels;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cels) {
+  //Converting from c to f is c * 9 / 5 + 32
+  let fahr = Math.round((cels * 9 / 5 + 32) * 10) / 10;
+  return fahr;
 };
 
 // Do not edit below this line
