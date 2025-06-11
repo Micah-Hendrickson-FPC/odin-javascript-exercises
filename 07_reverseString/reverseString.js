9ca2d188d31e241469c17d5a21b3bc4fd5bd5a59
Micah-Hendrickson-FPC <micah.hendrickson@fivepackcreative.com>
2025-06-11 15:34:24 -0500

    Implement string reversal method

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(userString) {
+    let reversedString = "";
+    for (let i = userString.length - 1; i >= 0; i--) {
+        reversedString += userString.charAt(i);
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
