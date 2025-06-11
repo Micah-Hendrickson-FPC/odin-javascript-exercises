48b4daaa87f89b23580da98b7fcdd1fade4a6345
Micah-Hendrickson-FPC <micah.hendrickson@fivepackcreative.com>
2025-06-11 15:20:38 -0500

    Create repeatString method.

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string, loops) {
+    let longString;
+    for (let i = 0; i < loops; i++) {
+        longString += string;
+    }
+    return longString;
 };
 
 // Do not edit below this line
