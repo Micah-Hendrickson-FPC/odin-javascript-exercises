237be64d49846e912963f583394913a6923868ed
Micah-Hendrickson-FPC <micah.hendrickson@fivepackcreative.com>
2025-06-11 15:44:07 -0500

    Create the removeFromArray method.

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,5 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...toRemove) {
+    return array.filter(value => !(toRemove.includes(value)));
 };
 
 // Do not edit below this line
