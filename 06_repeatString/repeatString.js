0814112a9c9e8cc6e670c08ed8d6fd81951a7925
Micah-Hendrickson-FPC <micah.hendrickson@fivepackcreative.com>
2025-06-11 15:25:10 -0500

    Update repeatString to error for negative loops

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,9 +1,15 @@
 const repeatString = function(string, loops) {
-    let longString;
-    for (let i = 0; i < loops; i++) {
-        longString += string;
-    }
-    return longString;
+    let result
+    if (loops < 0) {
+        result = "ERROR"
+    } else {
+        let longString = "";
+        for (let i = 0; i < loops; i++) {
+            longString += string;
+        }
+        result = longString
+    }  
+    return result;
 };
 
 // Do not edit below this line
